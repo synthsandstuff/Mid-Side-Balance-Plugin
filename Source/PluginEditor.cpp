77f0f4a5578202dade2c221761da895f9c1f1789
D Eatch <34381661+synthsandstuff@users.noreply.github.com>
2022-04-09 19:04:18 +0100

    Add files via upload

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -16,13 +16,13 @@ MidSideAudioProcessorEditor::MidSideAudioProcessorEditor (MidSideAudioProcessor&
     setSize (200, 600);
     
     midGain.setSliderStyle(juce::Slider::LinearBarVertical);
-    midGain.setRange(-100, +12, 0.5);
-    midGain.setTextValueSuffix (" dB");
+    midGain.setRange(-144, +12, 0.5);
+    midGain.setTextValueSuffix(" dB");
     midGain.setTextBoxStyle(juce::Slider::TextBoxAbove, true, 40, 30);
     
     sideGain.setSliderStyle(juce::Slider::LinearBarVertical);
-    sideGain.setRange(-100, +12, 0.5);
-    sideGain.setTextValueSuffix (" dB");
+    sideGain.setRange(-144, +12, 0.5);
+    sideGain.setTextValueSuffix(" dB");
     sideGain.setTextBoxStyle(juce::Slider::TextBoxAbove, true, 40, 30);
     
     addAndMakeVisible(&midGain);
@@ -36,7 +36,7 @@ MidSideAudioProcessorEditor::MidSideAudioProcessorEditor (MidSideAudioProcessor&
     audioProcessor.sideGainVal = juce::Decibels::decibelsToGain((float)sideGain.getValue());
     sideGain.setValue(juce::Decibels::gainToDecibels(audioProcessor.sideGainVal));
     sideGain.onValueChange  = [this] {audioProcessor.sideGainVal = juce::Decibels::decibelsToGain((float)sideGain.getValue());};
-    
+/*
     addAndMakeVisible(midLabel);
     midLabel.setText("Mid Gain", juce::dontSendNotification);
     midLabel.attachToComponent(&midGain, true);
@@ -44,6 +44,7 @@ MidSideAudioProcessorEditor::MidSideAudioProcessorEditor (MidSideAudioProcessor&
     addAndMakeVisible(sideLabel);
     sideLabel.setText("Side Gain", juce::dontSendNotification);
     sideLabel.attachToComponent(&sideGain, true);
+ */
 }
 
 MidSideAudioProcessorEditor::~MidSideAudioProcessorEditor()
@@ -55,22 +56,22 @@ MidSideAudioProcessorEditor::~MidSideAudioProcessorEditor()
 void MidSideAudioProcessorEditor::paint (juce::Graphics& g)
 {
     g.fillAll (juce::Colours::black);
-    g.setColour (juce::Colours::black);
+    g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Mid Side Gain", 100, 0, getWidth(), 30, juce::Justification::centred, 1);
+    g.drawFittedText ("Mid Side Gain", 0, 0, getWidth(), 30, juce::Justification::centred, 1);
 }
 
 void MidSideAudioProcessorEditor::resized()
 {
     midGain.setBounds(50, 50, 40, 500);
     sideGain.setBounds(100, 50, 40, 500);
-    midLabel.setBounds (50, 580, getWidth(), 20);
-    sideLabel.setBounds (100, 580, getWidth(), 20);
+/*  midLabel.setBounds (50, 580, 40, 20);
+    sideLabel.setBounds (100, 580, 40, 20);*/
 }
 
 double getValueFromText(const juce::String& text)
 {
-    auto minusInfinitydB = -100.0;
+    auto minusInfinitydB = -144.0;
     auto decibelText = text.upToFirstOccurrenceOf("dB", false, false).trim();
     return decibelText.equalsIgnoreCase("-INF") ? minusInfinitydB
                                                 : decibelText.getDoubleValue();
